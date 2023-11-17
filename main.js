aac0145c283116605f906423961db1d25b038f7e
annalaura008 <otavioasnhack@gmail.com>
2023-11-17 14:49:28 -0300

    Ex. 4 concluído com sucesso

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ else {
     console.log(num2)
 }
 */
-let num1 = 1500
+/*let num1 = 1500
 let num2 = 80
 let num3 = 420
 if(num1 > num2 && num1 > num3){
@@ -29,5 +29,19 @@ else if(num2 > num1 && num2 > num3){
 }
 else{
     console.log(num3)
-}
+}*/
 
+const anguloint1 = 60
+const anguloint2 = 60
+const anguloint3 = 60
+const soma = anguloint1 + anguloint2 + anguloint3
+
+if(soma == 180){
+    console.log(true)
+}
+else if(soma <=0 || soma >=360){
+console.log('Valor inválido')
+}
+else{
+    console.log(false)
+}*
