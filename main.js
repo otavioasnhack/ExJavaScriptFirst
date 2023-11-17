ea2dcd1a65110b884bc1612eff2e0aae0ba0eee1
annalaura008 <otavioasnhack@gmail.com>
2023-11-17 13:45:40 -0300

    exercicios 1,2 e 3 concluidos

---
+++ main.js
@@ -0,0 +1,33 @@
+/*
+const num1 = 15
+const num2 = 8
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+*/
+/*
+let num1 = 15
+let num2 = 80
+if(num1 > num2){
+    console.log(num1)
+}
+else {
+    console.log(num2)
+}
+*/
+let num1 = 1500
+let num2 = 80
+let num3 = 420
+if(num1 > num2 && num1 > num3){
+    console.log(num1)
+}
+else if(num2 > num1 && num2 > num3){
+    console.log(num2)
+}
+else{
+    console.log(num3)
+}
+
